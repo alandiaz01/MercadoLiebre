50928ec3840166fd6b089c083050fbe9b3aeef9b
alandiaz01 <alandiazyoel1@gmail.com>
2022-08-31 20:01:10 -0300

    reload

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.listen(process.env.PORT || 3000, ()=>{
 /*process.env.PORT || */
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/views/home.html'));
+    res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/views/login.html'));
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(__dirname + '/views/register.html');
 });
 
 
